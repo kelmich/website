9d5b278d19587bf5cddeb0f366b5e54df4a7b939
Michael <michaelkeller3000@gmail.com>
2021-02-07 13:39:45 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  siteMetadata: {
+    title: "website",
+  },
+  plugins: [
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-offline",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
