305a3d5b07eda80a8e99f0cb79b5206bd98875a0
Michael Keller <mail@kellermichael.com>
2023-03-03 20:42:55 +0100

    Update Website

--- src/Components/MyPlan.tsx
+++ src/Components/MyPlan.tsx
@@ -4,7 +4,7 @@ import { Timeline, Text } from "@mantine/core";
 function MyPlan() {
   return (
     <div style={{ width: 300 }}>
-      <Timeline active={1} bulletSize={20} lineWidth={5}>
+      <Timeline active={2} bulletSize={20} lineWidth={5}>
         <Timeline.Item title="Kantonsschule Frauenfeld">
           <Text color="dimmed" size="sm">
             Completed Swiss Matura with focus Law and Economics and thesis in
@@ -14,19 +14,19 @@ function MyPlan() {
 
         <Timeline.Item title="ETH Zürich">
           <Text color="dimmed" size="sm">
-            Currently in my 6th semester of my BSc in Computer Science.
+            Requirements for BSc Computer Science satisfied.
           </Text>
         </Timeline.Item>
 
-        <Timeline.Item title="Swiss Armed Forces" lineVariant="dashed">
+        <Timeline.Item title="Swiss Armed Forces">
           <Text color="dimmed" size="sm">
             In the Winter of 2023 I will complete my Swiss Army service.
           </Text>
         </Timeline.Item>
 
-        <Timeline.Item title="Internship" lineVariant="dashed">
+        <Timeline.Item title="ETH Zürich">
           <Text color="dimmed" size="sm">
-            I'm looking for an Internship Opportunity in IT.
+            Master Studies at ETH Zürich.
           </Text>
         </Timeline.Item>
       </Timeline>
