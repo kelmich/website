5a4cd44c1d458d05bc3ff0c44285ebe537916ee8
Michael Keller <mail@kellermichael.com>
2025-06-03 17:43:00 +0200

    savepoint css fix

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        "hind-madurai": ["HindMadurai", "serif"],
+      },
+    },
+  },
+} satisfies Config;
