9c6a995b7b9115e96d14ced9bb338253c82cd332
Michael Keller <mail@kellermichael.com>
2025-06-03 17:28:48 +0200

    reset stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import React from "react";
 import "./globals.css";
-import { ThemeProvider } from "./components/ThemeProvider";
-import ClientWrapper from "./components/ClientWrapper";
 
 export const metadata: Metadata = {
   title: "Michael Keller",
@@ -16,11 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>
-        <ThemeProvider>
-          <ClientWrapper>{children}</ClientWrapper>
-        </ThemeProvider>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,9 @@
 import React from "react";
-import AboutMe from "./components/AboutMe";
-import MyPlan from "./components/MyPlan";
 
 export default function Home() {
   return (
-    <div className="bg-gray-100 dark:bg-gray-900 min-h-screen flex flex-col items-center justify-center transition-colors duration-300">
-      <div className="flex items-center justify-center mt-5">
-        <h1 className="text-4xl md:text-5xl font-bold font-hind-madurai text-gray-900 dark:text-white">
-          Michael Keller
-        </h1>
-      </div>
-
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-8 max-w-6xl w-full px-4">
-        <div className="flex justify-center">
-          <AboutMe />
-        </div>
-
-        <div className="flex items-stretch justify-center">
-          <div className="flex flex-col items-left justify-between p-5 pt-10">
-            <MyPlan />
-          </div>
-        </div>
-      </div>
+    <div className="bg-gray-100 dark:bg-gray-900 min-h-screen flex flex-col items-center justify-center">
+      Hey! I&apos;m Michael
     </div>
   );
 }
