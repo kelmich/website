7b7ab53d00fbcfd6a8c2abe3b1c2f2b4de5076fa
Michael Keller <michael@deepjudge.ai>
2025-07-22 00:17:26 +0200

    short conclusion

--- content/blog/flatfalcon/9-conclusion/page.tsx
+++ content/blog/flatfalcon/9-conclusion/page.tsx
@@ -4,6 +4,20 @@ export default async function Home() {
       <h1>Chapter 9</h1>
       <h2>Conclusion</h2>
 
+      <p>
+        We have seen a couple different algorithms now for solving our problem.
+        There are different tradeoffs to be made. As we saw it mostly comes down
+        to the need for query time speed vs memory usage and resources we can
+        muster to precompute some auxilliary datastructures.
+      </p>
+
+      <p>
+        For my project I ended up choosing the Hub Label approach. While it is
+        very performant, a very attractive property it has is that it can be
+        implemented simply in SQL. We do not need a separate datastructure or
+        service from where we would store our listings.
+      </p>
+
       <p>Thank you for reading this blog post. I hope you enjoyed it!</p>
     </>
   );
